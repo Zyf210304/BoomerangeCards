c02e957925ce1e95fd9f4fade2ee5c47bc2dc5ce
张亚飞 <672233580@qq.com>
2022-12-15 18:35:46 +0800

    Initial Commit

---
+++ BoomerangeCards/BoomerangeCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoomerangeCardsApp.swift
+//  BoomerangeCards
+//
+//  Created by 张亚飞 on 2022/12/15.
+//
+
+import SwiftUI
+
+@main
+struct BoomerangeCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
